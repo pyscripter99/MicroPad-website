9a687edfa91cce848343d7598b94918ce408b432
pyscripter99 <retzerrt@gmail.com>
2024-01-13 19:00:37 +1000

    Add preprocessor

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,28 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import UnoCSS from "unocss/vite";
 import extractorSvelte from "@unocss/extractor-svelte";
+import { svelteSVG } from "rollup-plugin-svelte-svg";
 
 export default defineConfig({
     plugins: [
         UnoCSS({
             extractors: [extractorSvelte()],
+        }),
+        svelteSVG({
+            enforce: "pre",
+            svgo: {
+                plugins: [
+                    {
+                        name: "preset-default",
+                        params: {
+                            overrides: {
+                                inlineStyles: false,
+                            },
+                        },
+                    },
+                    "removeStyleElement",
+                ],
+            },
         }),
         sveltekit(),
     ],
